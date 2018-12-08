b5f3f2c7132869c751ec6362dc57b6f53ce4e083
leetwito <leetwito@gmail.com>
2018-12-08 23:18:17 +0200

    saves pd to csv. need to implement generators and flow from directory

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[10]:
 
 
 import scipy.io 
@@ -12,12 +12,16 @@ from glob import glob
 import numpy as np
 from tqdm import tqdm
 import os
+from PIL import Image
+import pandas as pd
 
-from keras.applications import vgg16, inception_v3, resnet50, mobilenet
+from keras.applications import mobilenet #, vgg16, inception_v3, resnet50, 
 from keras.optimizers import Adam
 from keras.utils import to_categorical
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
 
+
+# ## Params
 
 # In[2]:
 
@@ -36,6 +40,8 @@ use_rgb = True # True=rgb, False=spectral
 window_size = (200, 200)
 shift = (100, 100)
 
+
+# ## utils
 
 # In[3]:
 
@@ -95,34 +101,66 @@ def create_crops_from_fileslist(fileslist, window_size, shift):
     labels = []
 
     for file in tqdm(fileslist):
+#         file_name = os.path.basename(file)
+#         print('Saving crops for file {} ...'.format(file_name))
 #         print(file)
         spectral, rgb = read_slide(file)
-        spectral_crops += create_batch_of_crops_from_slide(spectral, window_size=window_size, shift=shift)
-        added_rgb_crops = create_batch_of_crops_from_slide(rgb, window_size=window_size, shift=shift)
-        rgb_crops += added_rgb_crops
-        file_name = os.path.basename(file)
-        if pos_name_init in file_name:
-            labels += [True]*len(added_rgb_crops)
-        elif neg_name_init in file_name:
-            labels += [False]*len(added_rgb_crops)
-        else:
-            raise ValueError('File {} is not in the right format ({}-pos, {}-neg)'.format(file_name, pos_name_init, neg_name_init))
+        spectral_crops = create_batch_of_crops_from_slide(spectral, window_size=window_size, shift=shift)
+        rgb_crops = create_batch_of_crops_from_slide(rgb, window_size=window_size, shift=shift)
+#         if pos_name_init in file_name:
+#             labels += [True]*len(added_rgb_crops)
+#         elif neg_name_init in file_name:
+#             labels += [False]*len(added_rgb_crops)
+#         else:
+#             raise ValueError('File {} is not in the right format ({}-pos, {}-neg)'.format(file_name, pos_name_init, neg_name_init))
 #         print(labels)
-    out_labels = to_categorical(labels)
-    out_sepc = np.stack(spectral_crops)
-    out_rgb = np.stack(rgb_crops)
+        save_dir = file.replace('.mat', '_win{}-{}_shift{}-{}'.format(window_size[0], window_size[1], shift[0], shift[1]))
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+#         np.save(save_dir+'/Spectral_crops.npy', spectral_crops)
+#         np.save(file.replace('.mat', '_RGB_win{}-{}_shift{}-{}.npy'.format(window_size[0], window_size[1], shift[0], shift[1])), rgb_crops)
+        for idx, (im_np, spec_np) in enumerate(zip(rgb_crops, spectral_crops)):
+            im = Image.fromarray(im_np)
+            im.save(os.path.join(save_dir, '{:05}.png'.format(idx)))
+            np.save(os.path.join(save_dir, '{:05}.npy'.format(idx)), spec_np)
+    
+#     out_labels = to_categorical(labels)
+#     out_sepc = np.stack(spectral_crops)
+#     out_rgb = np.stack(rgb_crops)
     
     
-    return out_sepc, out_rgb, out_labels
+#     return out_sepc, out_rgb, out_labels
 
 
 # In[7]:
 
 
 def create_crops_from_dir(dir_path, window_size, shift):
-    fileslist = glob(dir_path + '/*')
-    spectral_crops, rgb_crops, labels = create_crops_from_fileslist(fileslist, window_size, shift)
-    return spectral_crops, rgb_crops, labels
+    print('Saving crops for slides in dir: {}'.format(dir_path))
+    fileslist = glob(dir_path + '/*.mat')
+    create_crops_from_fileslist(fileslist, window_size, shift)
+#     return spectral_crops, rgb_crops, labels
+
+
+# In[35]:
+
+
+def create_csv_for_folder(data_dir, ext):
+    if ext[0] == '.':
+        ext = ext[1:]
+    data_df = pd.DataFrame(columns=['filename', 'label'])
+    files = glob(os.path.join(data_dir,'*', '*.{}'.format(ext)))
+#     print(data_dir+'/*/*.{}'.format(ext))
+    
+    init_len = len(data_dir)
+    files = [file[init_len:] for file in files]
+#     print(files)
+    labels = [1 if pos_name_init in file else 0 for file in files]
+#     print(labels)
+    data_df['filename'] = files
+    data_df['label'] = labels
+    data_df.to_csv(os.path.join(data_dir, os.path.basename(data_dir)+'.csv'), index=False)
+    print('Created CSV successfully for folder {}'.format(data_dir))
 
 
 # ## test and vis
@@ -132,26 +170,23 @@ img = rgb
 
 crops = create_batch_of_crops_from_slide(img, window_size=window_size, shift=shift, vis_flag=True)plt.imshow(img)
 # ## prepare data
-
-# In[8]:
-
-
-train_spectral, train_rgb, train_labels = create_crops_from_dir(data_dir+'/Train', window_size=window_size, shift=shift)
+train_spectral, train_rgb, train_labels = create_crops_from_dir(data_dir+'/Train', window_size=window_size, shift=shift)test_spectral, test_rgb, test_labels = create_crops_from_dir(data_dir+'/Test', window_size=window_size, shift=shift)
+eval_spectral, eval_rgb, eval_labels = create_crops_from_dir(data_dir+'/Eval', window_size=window_size, shift=shift)create_crops_from_dir(data_dir, window_size=window_size, shift=shift)
+# In[37]:
 
 
-# In[9]:
+ext = 'png'
+split_data_dir = 'E:\Datasets\PathoBarIlan'
+create_csv_for_folder(split_data_dir+'/Train', ext)
 
+print('Data:')
+train_pos = train_labels[:,1].sum()
+eval_pos = eval_labels[:,1].sum()
+test_pos = test_labels[:,1].sum()
 
-test_spectral, test_rgb, test_labels = create_crops_from_dir(data_dir+'/Test', window_size=window_size, shift=shift)
-eval_spectral, eval_rgb, eval_labels = create_crops_from_dir(data_dir+'/Eval', window_size=window_size, shift=shift)
-
-
-# In[10]:
-
-
-print(eval_labels, train_labels, test_labels)
-
-
+print('Train: {}/{} (pos/neg)'.format(train_pos, len(train_labels)-train_pos ))
+print('Test: {}/{} (pos/neg)'.format(test_pos, len(test_labels)-test_pos ))
+print('Eval: {}/{} (pos/neg)'.format(eval_pos, len(eval_labels)-eval_pos ))
 # ##### old prepare data
 train_pos_spectral_crops, train_pos_rgb_crops = create_crops_from_fileslist(cancer_paths[:-3], window_size=window_size, shift=shift)
 test_pos_spectral_crops, test_pos_rgb_crops = create_crops_from_fileslist(cancer_paths[-3:-1])
@@ -178,7 +213,7 @@ y_val = np.array(y_pos_val + y_neg_val)# print(len(y_train))
 # print(len(train_X_rgb))
 # ## build and train model
 
-# In[11]:
+# In[ ]:
 
 
 if use_rgb:
@@ -194,33 +229,19 @@ y_eval = eval_labels
 y_test = test_labels
 
 
-# In[13]:
-
-
-print('Data:')
-print(y_train.shape)
-train_pos = y_train[:,0].sum()
-eval_pos = y_eval[:,0].sum()
-test_pos = y_test[:,0].sum()
-
-print('Train: {}/{} (pos/neg)'.format(train_pos, len(y_train)-train_pos ))
-print('Test: {}/{} (pos/neg)'.format(test_pos, len(y_test)-test_pos ))
-print('Eval: {}/{} (pos/neg)'.format(eval_pos, len(y_eval)-eval_pos ))
-
-
-# In[14]:
+# In[ ]:
 
 
 mobilenet_model = mobilenet.MobileNet(include_top=True, weights=None, input_shape=x_train[0].shape, classes=2, dropout=0.2)
 
 
-# In[15]:
+# In[ ]:
 
 
 mobilenet_model.summary()
 
 
-# In[16]:
+# In[ ]:
 
 
 optimizer = Adam(lr=1e-3)
@@ -229,44 +250,44 @@ lrReduce = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=4, verbose
 chkpnt = ModelCheckpoint("my_models/model_spec", save_best_only=True)
 
 
-# In[17]:
+# In[ ]:
 
 
 print(x_train.shape, y_train.shape)
 print(x_eval.shape, y_eval.shape)
 
 
-# In[18]:
+# In[ ]:
 
 
 mobilenet_model.fit(x=x_train, y=y_train, epochs=1000, validation_data=(x_eval, y_eval), batch_size=64, verbose=2, callbacks=[chkpnt, lrReduce], shuffle=True)
 
 
-# In[19]:
+# In[ ]:
 
 
 y_pred = mobilenet_model.predict(test_rgb)
 
 
-# In[20]:
+# In[ ]:
 
 
 y_pred = y_pred[:, 0]>y_pred[:, 1]
 
 
-# In[21]:
+# In[ ]:
 
 
 y_test = y_test[:, 0]>y_test[:, 1]
 
 
-# In[22]:
+# In[ ]:
 
 
 (y_test.T[0] != y_pred).sum()
 
 
-# In[23]:
+# In[ ]:
 
 
 len(y_test)
